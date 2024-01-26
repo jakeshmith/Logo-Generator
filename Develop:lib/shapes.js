215440fbf4263e6b0a3e9baf8e6631561b477b45
jakeshmith <153260507+jakeshmith@users.noreply.github.com>
2024-01-25 19:42:57 -0600

    added js for shapes

---
+++ Develop:lib/shapes.js
@@ -0,0 +1,28 @@
+class Shape{
+    constructor (shape, color){
+        this.color=''
+    }
+    setColor(color){
+        this.color=(color);
+    }
+}
+
+class Circle extends Shape{
+    createShape() {
+        return `<circle cx="150" cy="100" r="80" fill="${this.color}">`
+    }
+}
+
+class Triangle extends Shape{
+    createShape() {
+        return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}">`
+    }
+}
+
+class Square extends Shape{
+    createShape() {
+        return `<rect x="90" y="40" width="120" height="120" fill="${this.color}">`
+    }
+}
+
+module.exports = Shape
